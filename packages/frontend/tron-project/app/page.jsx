26952847777e4298ba11bc9fd7033797a8f10e1c
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-03-15 14:23:12 +0300

    animation added

--- packages/frontend/tron-project/app/page.jsx
+++ packages/frontend/tron-project/app/page.jsx
@@ -1,48 +1,92 @@
-"use client"
+"use client";
 import React from "react";
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
-  const images = [
-   'https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-   'https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-   'https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-   'https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-   'https://images.unsplash.com/photo-1530076886461-ce58ea8abe24?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-   'https://images.unsplash.com/photo-1570051008600-b34baa49e751?q=80&w=1185&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ];
+const images = [
+  "https://images.unsplash.com/photo-1505533321630-975218a5f66f?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1591779051696-1c3fa1469a79?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1626808642875-0aa545482dfb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=1174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1530076886461-ce58ea8abe24?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1570051008600-b34baa49e751?q=80&w=1185&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+
+const variants = {
+  initial: (direction) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+
+    };
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+
+    transition: {
+      x: { type: "spring", stiffness: 300, damping: 30 },
+      opacity: { duration: 0.2 },
+    },
+  },
+  exit: (direction) => {
+    return {
+      x: direction > 0 ? -1000 : 1000,
+      opacity: 0,
+
+      transition: {
+        x: { type: "spring", stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      },
+    };
+  },
+};
+
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const [direction, setDirection] = useState(0);
 
-const [index, setIndex] = useState(0);
-
-  
-    function nextStep(){
-      if (index === images.length - 1) {
-        setIndex(0)
-        return
-      }
-      setIndex(index + 1)
+  function nextStep() {
+    setDirection(1);
+    if (index === images.length - 1) {
+      setIndex(0);
+      return;
     }
+    setIndex(index + 1);
+  }
 
-    function prevStep(){
-      if(index === 0){
-        setIndex(images.length - 1)
-        return
-      }
-      setIndex(index - 1)
+  function prevStep() {
+    setDirection(-1);
+    if (index === 0) {
+      setIndex(images.length - 1);
+      return;
     }
-  
+    setIndex(index - 1);
+  }
 
   return (
     <div className="bg-gradient-to-r flex items-center justify-center from-purple-950 to-violet-600 min-h-screen">
-      
-      
-        <div className="slideshow">
-          <img src={images[index]} alt="slides" className="slides" />
-          <button className="prevButton" onClick={prevStep}>◀</button>
-          <button className="nextButton" onClick={nextStep}>▶</button>
-        </div>
-      
-
+      <div className="slideshow">
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.img
+            variants={variants}
+            animate="animate"
+            initial="initial"
+            exit="exit"
+            src={images[index]}
+            alt="slides"
+            className="slides"
+            key={images[index]}
+            custom={direction}
+          />
+        </AnimatePresence>
+        <button className="prevButton" onClick={prevStep}>
+          ◀
+        </button>
+        <button className="nextButton" onClick={nextStep}>
+          ▶
+        </button>
+      </div>
     </div>
   );
 }

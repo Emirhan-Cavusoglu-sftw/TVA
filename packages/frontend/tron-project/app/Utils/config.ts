07df904327f371f5e22737de9971ba70390e1560
Emirhan Cavusoglu <116461155+Emirhan-Cavusoglu-sftw@users.noreply.github.com>
2024-05-05 00:08:02 +0300

    Delete packages/frontend/tron-project/app/Utils directory

--- packages/frontend/tron-project/app/Utils/config.ts
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from "wagmi";
-import { scrollSepolia } from "wagmi/chains";
-
-export const config = createConfig({
-  chains: [scrollSepolia],
-  transports: {
-    [scrollSepolia.id]: http("https://rpc.ankr.com/scroll_sepolia_testnet/d17775fb78762b92aacf9f30af7ccaac0c4e758d5bb9f2ebc3faef3b9cbed604"),
-  },
-});

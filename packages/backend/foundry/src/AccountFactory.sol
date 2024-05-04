8ebdb1c25033022f28d9517202c39bb683926fa1
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-05-05 01:09:22 +0300

    Account factory update

--- packages/backend/foundry/src/AccountFactory.sol
+++ packages/backend/foundry/src/AccountFactory.sol
@@ -20,4 +20,31 @@ contract AccountFactory {
 
         return address(account);
     }
+    // function that gets all accounts and in all accounts it calls getTsds function and pushes the result to an array
+    function getAllTsds() public view returns (address[] memory) {
+    // Calculate the total number of TSDs
+    uint totalTsdCount = 0;
+    for (uint256 i = 0; i < accounts.length; i++) {
+        Account account = Account(accounts[i]);
+        totalTsdCount += account.getTsds().length;
+    }
+
+    // Initialize a single array to store all TSD addresses
+    address[] memory allTsds = new address[](totalTsdCount);
+
+    // Copy TSD addresses from all accounts into the single array
+    uint256 currentIndex = 0;
+    for (uint256 i = 0; i < accounts.length; i++) {
+        Account account = Account(accounts[i]);
+        address[] memory tsds = account.getTsds();
+
+        for (uint256 j = 0; j < tsds.length; j++) {
+            allTsds[currentIndex] = tsds[j];
+            currentIndex++;
+        }
+    }
+
+    return allTsds;
+}
+
 }
